b17284c53aa7b56e157ee3ecc8e75fc2bb5225f9
2Sodol <104712554+dex7688@users.noreply.github.com>
2022-11-09 21:49:07 +0900

    modal css

--- js/store.js
+++ js/store.js
@@ -1,16 +1,15 @@
-const desc = document.querySelectorAll(".desc");
-const open = document.querySelectorAll(".open");
-const close = document.querySelectorAll(".close");
+const desc = document.querySelectorAll('.desc');
+const open = document.querySelectorAll('.open');
+const close = document.querySelectorAll('.close');
 
 open.forEach((item, index) => {
   item.onclick = () => {
-    desc[index].classList.toggle("active");
+    desc[index].classList.toggle('active');
   };
 });
 
 close.forEach((item, index) => {
   item.onclick = () => {
-    desc[index].classList.remove("active");
-
+    desc[index].classList.remove('active');
   };
 });
